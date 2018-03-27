0cdd0b1a8b8957e312d831ad451b10edb7aaa8eb
Sean O'Brien <seanmobrien@hotmail.com>
2018-03-26 23:09:48 -0500

    GetLogs returning records 1000 at a time Site infrastructure prepared

---
+++ src/CryptoKitties.Net.Services.AccountObserver.Interfaces/System/Fabric/Description/FabricDescriptionExtensionMethods.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System.Fabric.Description
+{
+    /// <summary>
+    /// Extends the <c>System.Fabric.Description</c> namespace.
+    /// </summary>
+    public static class FabricDescriptionExtensionMethods
+    {
+        /// <summary>
+        /// Reads a <see cref="TimeSpan"/> from <paramref name="config"/>.
+        /// </summary>
+        /// <param name="config">The <see cref="ConfigurationSection"/> being extended.</param>
+        /// <param name="name">A <see cref="string"/> that contains the name of the vluae.</param>
+        /// <param name="defaultValue">A <typparamref name="TValue"/> value used as default.</param>
+        /// <returns>The resultig <see cref="TimeSpan"/></returns>
+        public static TimeSpan ReadValue(this ConfigurationSection config, string name, TimeSpan defaultValue)
+        {
+            TimeSpan ret;
+            var input = config.ReadValue(name);
+            return input != null && TimeSpan.TryParse(input, out ret)
+                ? ret
+                : defaultValue;
+        }
+        /// <summary>
+        /// Reads a <see cref="uint"/> from <paramref name="config"/>.
+        /// </summary>
+        /// <param name="config">The <see cref="ConfigurationSection"/> being extended.</param>
+        /// <param name="name">A <see cref="string"/> that contains the name of the vluae.</param>
+        /// <param name="defaultValue">A <typparamref name="TValue"/> value used as default.</param>
+        /// <returns>The resultig <see cref="TimeSpan"/></returns>
+        public static uint ReadValue(this ConfigurationSection config, string name, uint defaultValue)
+        {
+            uint ret;
+            var input = config.ReadValue(name);
+            return input != null && uint.TryParse(input, out ret)
+                ? ret
+                : defaultValue;
+        }
+        /// <summary>
+        /// Reads a <see cref="TimeSpan"/> from <paramref name="config"/>.
+        /// </summary>
+        /// <param name="config">The <see cref="ConfigurationSection"/> being extended.</param>
+        /// <param name="name">A <see cref="string"/> that contains the name of the vluae.</param>
+        /// <param name="defaultValue">A <typparamref name="TValue"/> value used as default.</param>
+        /// <returns>The resultig <see cref="TimeSpan"/></returns>
+        public static string ReadValue(this ConfigurationSection config, string name, string defaultValue = default(string))
+        {
+            var param = config.Parameters.FirstOrDefault(x => x.Name == name);
+            return param == null
+                ? defaultValue
+                : param.Value;
+        }
+    }
+}
---
+++ src/CryptoKitties.Net.Services.TransactionPublisher/TransactionPublisher.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Fabric;
+using System.Fabric.Description;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.ServiceFabric.Data.Collections;
+using Microsoft.ServiceFabric.Services.Communication.Runtime;
+using Microsoft.ServiceFabric.Services.Runtime;
+using Nethereum.Contracts;
+using Nethereum.RPC.Eth.DTOs;
+using Nethereum.Web3;
+
+namespace CryptoKitties.Net.Services.TransactionPublisher
+{
+    /// <summary>
+    /// An instance of this class is created for each service instance by the Service Fabric runtime.
+    /// </summary>
+    internal sealed class TransactionPublisher : StatefulService
+    {
+        public TransactionPublisher(StatefulServiceContext context)
+            : base(context)
+        { }
+
+
+        private Web3 Web3 { get; set; }
+        private string ContractAddress { get; set; }
+
+        private Event SaleEvent { get; set; }
+        private Event AuctionEvent { get; set; }
+
+
+
+        /// <summary>
+        /// Optional override to create listeners (e.g., HTTP, Service Remoting, WCF, etc.) for this service replica to handle client or user requests.
+        /// </summary>
+        /// <remarks>
+        /// For more information on service communication, see https://aka.ms/servicefabricservicecommunication
+        /// </remarks>
+        /// <returns>A collection of listeners.</returns>
+        protected override IEnumerable<ServiceReplicaListener> CreateServiceReplicaListeners()
+        {
+            /*
+            var listenerSettings = new FabricTransportRemotingListenerSettings
+            {
+                MaxMessageSize = 10000000,
+                SecurityCredentials = RemotingExtensions.GetSecurityCredentials(
+                    RemotingExtensions.Certificates.ServiceThumbprint,
+                    RemotingExtensions.Certificates.WebsiteThumbprint,
+                    RemotingExtensions.Certificates.WebsiteCommonName
+                )
+            };
+            return new[] {
+                new ServiceReplicaListener(context => new FabricTransportServiceRemotingListener(context, this, listenerSettings))
+            };
+            */
+            return new ServiceReplicaListener[0];
+        }
+        /// <summary>
+        /// This is the main entry point for your service instance.
+        /// </summary>
+        /// <param name="cancellationToken">Canceled when Service Fabric needs to shut down this service instance.</param>
+        protected override async Task RunAsync(CancellationToken cancellationToken)
+        {
+            await OnActivateAsync();
+
+            var doAgain = true;
+            while (doAgain)
+            {
+                var logData = await GetLogs();
+
+                // TODO: Process logs
+            }
+             
+        }
+
+        async Task<object> GetLogs()
+        {
+            throw new Exception();
+        }
+
+
+
+
+
+
+        class StateKeys
+        {
+            public const string CurrentBlock = "CurrentBlock";
+        }
+        async Task OnActivateAsync()
+        {
+            var configurationPackage = Context.CodePackageActivationContext.GetConfigurationPackageObject("Config");
+            var config = configurationPackage.Settings.Sections["TransactionPublisherConfig"];
+
+            Web3 = new Web3(config.ReadValue("EthereumConnection"));
+
+            var sales = Web3.Eth.GetContract(Globals.Contracts.Address.SalesAuction,
+                Globals.Contracts.ABI.SalesAuction);
+            sales.GetEvent("xxx");
+
+
+        }
+    }
+}
